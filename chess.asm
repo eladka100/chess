387a3554c7b3fee8d753690e50cc3c416f6311f1
eladka100 <96748792+eladka100@users.noreply.github.com>
2023-04-09 00:56:07 +0300

    addes inital position

--- chess.asm
+++ chess.asm
@@ -3,9 +3,31 @@ MOODEL small
 STACK 100h
 DATASEG
 
+;piece pos format: (1 2) (3 4 5) (6 7 8)
+;            		/       |       | 
+;           pawn promotion file    row
+;
 
+Wpawns db 000001b, 001001b, 010001b, 011001b, 100001b, 101001b, 110001b, 111001b
+Wrooks db 000000b, 111000b
+Wknites db 001000b, 110000b
+Wbishops db 010000b, 101000b
+Wqueen db 011000b
+Wking db 100000b
+
+Bpawns db 000110b, 001110b, 010110b, 011110b, 100110b, 101110b, 110110b, 1110110b
+Brooks db 000111b, 111111b
+Bknites db 001111b, 110111b
+Bbishops db 010111b, 101111b
+Bqueen db 011111b
+Bking db 100111b
 
 CODESEG
+
+
+
+
+
 start:
 	mov ax, @data
 	mov ds, ax
