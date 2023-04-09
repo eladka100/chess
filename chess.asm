f0379a24706ba733ffc61a5f22983118379f940a
eladka100 <96748792+eladka100@users.noreply.github.com>
2023-04-09 15:52:32 +0300

    done with painting the

--- chess.asm
+++ chess.asm
@@ -106,12 +106,14 @@ Sking db "tttttttttttttttt"
 
 	   
 		
-sprites dw 8 dup(offset Spawn), 2 dup(offset Srook), 2 dup(offset Sknight), 2 dup(offset Sbishop), offset Squeen, offset Sking
+sprites dw 8 dup(offset promotionSprites), 2 dup(offset Srook), 2 dup(offset Sknight), 2 dup(offset Sbishop), offset Squeen, offset Sking
 
 ;piece pos format: 1  2 (3 4 5) (6 7 8)
 ;            	     /     |       | 
 ;                 eaten   file    row
 ;             
+
+
 
 Wpawns db 000001b, 001001b, 010001b, 011001b, 100001b, 101001b, 110001b, 111001b
 Wrooks db 000000b, 111000b
@@ -127,9 +129,25 @@ Bbishops db 010111b, 101111b
 Bqueen db 011111b
 Bking db 100111b
 
+;promotions: 0 - pawn, 1 - rook, 2 - knight, 3 - bishop, 4 - queen
+
+
+Wpromotions db 8 dup(00h)
+Bpromotions db 8 dup (00h)
+promotionSprites dw offset Spawn, offset Srook, offset Sknight, offset Sbishop, offset Squeen
+
 Xp dw 0000h
 Yp dw 0000h
 color db 00h
+
+
+;piece numbers: 0-7 - pawn, 8-9 - rook, A-B - knight, C-D - bishop, E - queen, F - king
+;piece format: 1 2 3 4 (5 6 7 8)
+;            	    /      |       
+;               color  piece num
+;  
+piece db 00h
+tile db 00h
 
 
 CODESEG
@@ -173,7 +191,7 @@ proc displayBoard ; displays the chess board (without the pieces)
 			mov cx, 0
 			mov dx, 0
 			jz whiteTile
-				mov [color], 16h ; it's black
+				mov [color], 00h ; it's black
 				jmp displayBoardPaintLoop
 			whiteTile:
 				mov [color], 0Fh ; it's white
@@ -247,7 +265,103 @@ proc background ; paint the background pattern
 	ret
 endp background
 
+proc paintPiece
+	push ax
+	push bx
+	push cx
+	push dx
+	push si
+	
+	mov ah, 0
+	mov al, [piece]
+	and al, 0Fh ; only look at the piece itself and not the color
+	shl al, 1
+	add ax, offset sprites
+	mov si, ax
+	mov si, [si] ; go to the start of the correct sprite
+	
+	mov bh, 0
+	mov bl, [piece]
+	add bx, offset Wpawns
+	mov al, [bx]
+	
+	push ax
+	and al, 40h
+	pop ax
+	jnz paintPieceHelp; the piece is eaten and so it won't be painted
+	
+	mov [tile], al
 
+	
+	
+	
+	mov ah, 0
+	mov al, [tile]
+	shr ax, 3 ; only look at the file
+	shl ax, 4 ; multiply by 16 because the tile size is 16 px
+	add ax, 96 ; add the offset of the board
+	mov [Xp], ax ; this is the X coordinate of the left corner of the piece
+	mov cx, ax ; save the X coordinate
+	
+	mov ah, 0
+	mov al, [tile]
+	and ax, 7 ; look at the row
+	shl ax, 4 ; multiply by 16 because the tile size is 16 px
+	neg ax ; the rows are in bottom to top but the coordinates are from top to bottom
+	add ax, 148 ; add the offset of the board
+	mov [Yp], ax ; this is the Y coordinate of the left corner of the piece
+	
+	
+	mov ax, 0
+	paintPieceLoop:
+			mov dl, [si]
+			cmp dl, 't'
+			je paintPieceCon ; if pixel should be transparent then don't do anything
+			
+			cmp dl, 'b'
+			jne insidePiece ; if it's a pixel in the border of the piece
+				mov [color], 16h
+				call putPixel
+				jmp paintPieceCon
+	
+	paintPieceHelp:
+	jmp paintPieceEnd
+	
+	
+			insidePiece: ; if it's a pixel in the inside of the piece
+				mov bl, [piece]
+				and bl, 10h ; check if the piece is white or black
+				jz whitePiece
+					mov [color], 00h
+					call putPixel
+					jmp paintPieceCon
+				whitePiece:
+					mov [color], 0Fh
+					call putPixel
+					jmp paintPieceCon
+			
+			paintPieceCon:
+			inc si
+			inc [Xp]
+			inc al
+			cmp al, 16
+			jb paintPieceLoop
+		mov [Xp], cx
+		mov al, 0
+		inc [Yp]
+		inc ah
+		cmp ah, 16
+		jb paintPieceLoop
+					
+	
+	paintPieceEnd:
+	pop si
+	pop dx
+	pop cx
+	pop bx
+	pop ax
+	ret
+endp paintPiece
 
 start:
 	mov ax, @data
@@ -258,6 +372,11 @@ start:
 	
 	call background
 	call displayBoard
+	
+	mov [piece], 0Eh
+	call paintPiece
+	
+	
 
 exit:
 	mov ah, 00h
